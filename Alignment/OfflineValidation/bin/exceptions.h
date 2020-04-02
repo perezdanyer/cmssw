f8ca61612521062c25e4b3f19de293defd4649c1
Patrick Connor <connorpa@mail.desy.de>
2020-04-02 11:21:31 +0200

    implementing tests

--- Alignment/OfflineValidation/bin/exceptions.h
+++ Alignment/OfflineValidation/bin/exceptions.h
@@ -10,9 +10,9 @@
 #include <boost/exception/diagnostic_information.hpp> 
 
 #include <boost/property_tree/exceptions.hpp>
-#include <boost/program_options/errors.hpp>
+#include <boost/filesystem.hpp>
+//#include <boost/program_options/errors.hpp>
 
-// TODO: filesystem error
 // TODO: utilise the exceptions provided by boost as much as possible
 
 using std::cerr;
@@ -31,7 +31,7 @@ public:
 
 inline std::string colorify (std::string s)
 {
-    const char * red = "\x1B[31m", /** green = "\x1B[32m",*/ * black = "\x1B[30m";
+    const char * red = "\x1B[31m"/*, * green = "\x1B[32m"*/ /*, * black = "\x1B[30m"*/;
     return red + s /*+ black*/; // TODO: clarify colours...
 }
 
@@ -40,15 +40,16 @@ template<int FUNC(int, char **)> int exceptions (int argc, char * argv[])
     try { return FUNC(argc, argv); }
     //catch (const AllInOneConfig::ConfigError & e) { cerr << "Config error: " << e.what() << '\n'; }
     catch (const boost::exception & e) { cerr << colorify("Boost exception: ") << boost::diagnostic_information(e); throw; }
-    catch (const boost::program_options::required_option &e) {
-        if (e.get_option_name() == "--config") cerr << colorify("Missing config") << '\n';
-        else cerr << colorify("Program Options Required Option: ") << e.what() << '\n';
-    }
-    catch (const boost::program_options::invalid_syntax & e) { cerr << colorify("Program Options Invalid Syntax: ") << e.what() << '\n'; }
-    catch (const boost::program_options::error          & e) { cerr << colorify("Program Options Error: ") << e.what() << '\n'; }
+    //catch (const boost::program_options::required_option &e) {
+    //    if (e.get_option_name() == "--config") cerr << colorify("Missing config") << '\n';
+    //    else cerr << colorify("Program Options Required Option: ") << e.what() << '\n';
+    //}
+    //catch (const boost::program_options::invalid_syntax & e) { cerr << colorify("Program Options Invalid Syntax: ") << e.what() << '\n'; }
+    //catch (const boost::program_options::error          & e) { cerr << colorify("Program Options Error: ") << e.what() << '\n'; }
     catch (const boost::property_tree::ptree_bad_data & e) { cerr << colorify("Property Tree Bad Data Error: ") << e.data<std::string>() << '\n'; }
     catch (const boost::property_tree::ptree_bad_path & e) { cerr << colorify("Property Tree Bad Path Error: ") << e.path<std::string>() << '\n'; }
     catch (const boost::property_tree::ptree_error    & e) { cerr << colorify("Property Tree Error: ") << e.what() << '\n'; }
+    catch (const boost::filesystem::filesystem_error & e) { cerr << colorify("Filesystem Error:" ) << e.what() << '\n'; }
     catch (const std::logic_error & e) { cerr << colorify("Logic Error: ")    << e.what() << '\n'; }
     catch (const std::exception   & e) { cerr << colorify("Standard Error: ") << e.what() << '\n'; }
     catch (...) { cerr << colorify("Unkown failure\n"); }
