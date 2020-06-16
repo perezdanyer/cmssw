dbe8328b65109e34e0c1a86a811c82dcb7d98c9f
David Brunner <david.brunner@desy.de>
2020-06-16 17:39:38 +0200

    Set default values properly

--- Alignment/OfflineValidation/bin/DMRmerge.cc
+++ Alignment/OfflineValidation/bin/DMRmerge.cc
@@ -48,6 +48,27 @@ int merge (int argc, char * argv[]){
     pt::ptree alignments = main_tree.get_child("alignments");
     pt::ptree validation = main_tree.get_child("validation");
 
+    //Read all configure variables and set default for missing keys
+    std::string methods = validation.get_child_optional("methods") ? getVecTokenized(validation, "methods", ",") : "median,rmsNorm";
+    std::string curves = validation.get_child_optional("curves") ? getVecTokenized(validation, "curves", ",") : "plain";
+
+    int minimum = validation.get_child_optional("minimum") ? validation.get<int>("minimum") : 30;
+
+    bool useFit = validation.get_child_optional("usefit") ? validation.get<bool>("usefit") : false;
+    bool bigText = validation.get_child_optional("bigtext") ? validation.get<bool>("bigtext") : false;
+
+    TkAlStyle::legendheader = validation.get_child_optional("legendheader") ? validation.get<std::string>("legendheader") : " ";
+    TkAlStyle::legendoptions = validation.get_child_optional("legendoptions") ? getVecTokenized(validation, "legendoptions", " ") : "mean";
+    TkAlStyle::set(INTERNAL, NONE, "", validation.get<std::string>("customrighttitle"));
+
+    std::vector<int> moduleids;
+
+    if(validation.get_child_optional("moduleid")){
+        for(std::pair<std::string, pt::ptree> childTree : validation.get_child("moduleid")){
+            moduleids.push_back(childTree.second.get_value<int>());
+        }
+    } 
+
     //Set configuration string for CompareAlignments class
     TString filesAndLabels;
 
@@ -62,32 +83,24 @@ int merge (int argc, char * argv[]){
     comparer.doComparison(filesAndLabels);
 
     //Create plots
-    TkAlStyle::legendheader = "";
-    TkAlStyle::legendoptions = getVecTokenized(validation, "legendoptions", " ");
-
-    TkAlStyle::set(INTERNAL, NONE, "", validation.get<std::string>("customrighttitle"));
-
     gStyle->SetTitleH(0.07);
     gStyle->SetTitleW(1.00);
     gStyle->SetTitleFont(132);
 
-    PlotAlignmentValidation plotter(false);
+    PlotAlignmentValidation plotter(bigText);
 
     for(std::pair<std::string, pt::ptree> childTree : alignments){
         plotter.loadFileList((childTree.second.get<std::string>("file") + "/DMR.root").c_str(), childTree.second.get<std::string>("title"), childTree.second.get<int>("color"), childTree.second.get<int>("style"));
     }
 
-    std::cout << getVecTokenized(validation, "curves", ",");
-
     plotter.setOutputDir(main_tree.get<std::string>("output"));
-    plotter.useFitForDMRplots(false);
+    plotter.useFitForDMRplots(useFit);
     plotter.setTreeBaseDir();
-    plotter.plotDMR(getVecTokenized(validation, "methods", ","), validation.get<int>("minimum"), getVecTokenized(validation, "curves", ","));
+    plotter.plotDMR(methods, minimum, curves);
     plotter.plotSurfaceShapes("TrackerOfflineValidation");
     plotter.plotChi2((main_tree.get<std::string>("output") + "/result.root").c_str());
 
-    vector<int> moduleids; // = {.oO[moduleid]Oo.};
-    for (auto moduleid : moduleids) {
+    for(const int& moduleid : moduleids) {
         plotter.residual_by_moduleID(moduleid);
     }
 
