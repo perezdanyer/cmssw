2a987b732655a443c3562f0f6d53b9ae7a3de995
Henriette Aarup Petersen <henriette.aarup.petersen@cern.ch>
2020-12-16 14:22:41 +0100

    add unit tests for PVs

---
+++ Alignment/OfflineValidation/test/test_PVsingleAndmerge.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+function die { echo $1: status $2 ; exit $2; }
+
+echo "TESTING Alignment/PV single configuration with json..."
+cd $CMSSW_BASE/src/Alignment/OfflineValidation/test/test_yaml_new/PV/single/testUnits/unitTest/317655/
+./cmsRun validation_cfg.py config=validation.json || die "Failure running PV single configuration with json" $?
+
+echo "TESTING Alignment/PV single configuration standalone..."
+./cmsRun validation_cfg.py || die "Failure running PV single configuration standalone" $?
+
+echo "TESTING PV merge step"
+cd $CMSSW_BASE/src/Alignment/OfflineValidation/test/test_yaml_new/PV/merge/testUnits/317655/
+./PVmerge validation.json || die "Failure running PV merge step" $?
