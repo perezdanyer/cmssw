ac117026ed85f6b43d4a20ef5e1c9580fa79d65f
Henriette Aarup Petersen <henriette.aarup.petersen@cern.ch>
2021-05-31 02:50:08 +0200

    adding trend unit test for DMRs, changing unit test dir and set iov to 1

--- Alignment/OfflineValidation/test/test_DMRsingleAndmerge.sh
+++ Alignment/OfflineValidation/test/test_DMRsingleAndmerge.sh
@@ -3,12 +3,16 @@
 function die { echo $1: status $2 ; exit $2; }
 
 echo "TESTING Alignment/DMR single configuration with json..."
-cd $CMSSW_BASE/src/Alignment/OfflineValidation/test/test_yaml_new/DMR/single/testUnits/unitTest/317655/
+cd $CMSSW_BASE/src/Alignment/OfflineValidation/test/test_yaml/DMR/single/testUnits/unitTest/1/
 ./cmsRun validation_cfg.py config=validation.json || die "Failure running DMR single configuration with json" $?
 
 echo "TESTING Alignment/DMR single configuration standalone..."
 ./cmsRun validation_cfg.py || die "Failure running DMR single configuration standalone" $?
 
 echo "TESTING DMR merge step"
-cd $CMSSW_BASE/src/Alignment/OfflineValidation/test/test_yaml_new/DMR/merge/testUnits/317655/
+cd $CMSSW_BASE/src/Alignment/OfflineValidation/test/test_yaml/DMR/merge/testUnits/1/
 ./DMRmerge validation.json || die "Failure running DMR merge step" $?
+
+echo "TESTING DMR trends"
+cd $CMSSW_BASE/src/Alignment/OfflineValidation/test/test_yaml/DMR/trends/testUnits/
+./DMRtrends validation.json --verbose || die "Failure running DMR trends" $?
