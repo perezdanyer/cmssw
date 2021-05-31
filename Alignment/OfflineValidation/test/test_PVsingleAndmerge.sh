71d897bb6b60031a5c0b38455440e33cd6eb5f27
Henriette Aarup Petersen <henriette.aarup.petersen@cern.ch>
2021-05-31 02:51:26 +0200

    add trend unit test for PV and change unit test dir from test_yaml_new to test_yaml

--- Alignment/OfflineValidation/test/test_PVsingleAndmerge.sh
+++ Alignment/OfflineValidation/test/test_PVsingleAndmerge.sh
@@ -3,12 +3,16 @@
 function die { echo $1: status $2 ; exit $2; }
 
 echo "TESTING Alignment/PV single configuration with json..."
-cd $CMSSW_BASE/src/Alignment/OfflineValidation/test/test_yaml_new/PV/single/testUnits/unitTest/1/
+cd $CMSSW_BASE/src/Alignment/OfflineValidation/test/test_yaml/PV/single/testUnits/unitTest/1/
 ./cmsRun validation_cfg.py config=validation.json || die "Failure running PV single configuration with json" $?
 
 echo "TESTING Alignment/PV single configuration standalone..."
 ./cmsRun validation_cfg.py || die "Failure running PV single configuration standalone" $?
 
 echo "TESTING PV merge step"
-cd $CMSSW_BASE/src/Alignment/OfflineValidation/test/test_yaml_new/PV/merge/testUnits/1/
+cd $CMSSW_BASE/src/Alignment/OfflineValidation/test/test_yaml/PV/merge/testUnits/1/
 ./PVmerge validation.json --verbose || die "Failure running PV merge step" $?
+
+echo "TESTING PV trends"
+cd $CMSSW_BASE/src/Alignment/OfflineValidation/test/test_yaml/PV/trends/testUnits/
+./PVtrends validation.json --verbose || die "Failure running PV trends" $?
